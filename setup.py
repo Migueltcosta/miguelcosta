18dfca01d9474a081848e4b53ac60dd27fed1f16
Migueltcosta <migueltecosta@gmail.com>
2020-05-19 12:38:14 +0100

    Second Commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='miguelcosta',
+      version='1.0',
+      description='Hello Distribution Module',
+      author='Migueltcosta',
+      author_email='Migueltcosta',
+      url='https://github.com/Migueltcosta',
+      py_modules=['miguelcosta'],
+     )
